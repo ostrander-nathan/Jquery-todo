fd359e699e6cfff669098ac20c4eeb129653fc64
Nathan Ostrander <ostrander_nathan@yahoo.com>
2016-11-02 19:18:32 -0500

    working except for logout

--- javascripts/firebaseMethods.js
+++ javascripts/firebaseMethods.js
@@ -1,18 +1,35 @@
 "use strict";
 
 var FbAPI = (function(oldFirebase){
-	
-	oldFirebase.getTodos = function (apiKeys) {
+	oldFirebase.getTodos = function (apiKeys, uid) {
 		return new Promise((resolve, reject) =>{
 			$.ajax({
 				method:"GET",
-				url:`${apiKeys.databaseURL}/items.json`
+				url:`${apiKeys.databaseURL}/items.json?orderBy="uid"&equalTo="${uid}"`
 			}).then((response)=>{
+				console.log("response",response );
 				let items = [];
 				Object.keys(response).forEach(function(key){ // exact code to use every firebase project
-					response[key].id = key;
-					items.push(response[key]);
+					response[key].id = key; //
+					items.push(response[key]); //
 				});
+				resolve(items);
+			},(error)=>{
+				reject(error);
+			});
+
+		});
+	};
+
+	oldFirebase.addTodo = function (apiKeys, newItem) {
+		return new Promise((resolve, reject) =>{
+			$.ajax({
+				method:"POST",
+				url:`${apiKeys.databaseURL}/items.json`,
+				data: JSON.stringify(newItem),
+				dataType:"json"
+			}).then((response)=>{
+				console.log("resolve from POST",response);
 				resolve(response);
 			},(error)=>{
 				reject(error);
@@ -21,6 +38,36 @@ var FbAPI = (function(oldFirebase){
 		});
 	};
 
+	oldFirebase.deleteTodo = function (apiKeys, itemId) {
+		return new Promise((resolve, reject) =>{
+			$.ajax({
+				method:"DELETE",
+				url:`${apiKeys.databaseURL}/items/${itemId}.json`,
+			}).then((response)=>{
+				console.log("resolve from Delete",response);
+				resolve(response);
+			},(error)=>{
+				reject(error);
+			});
+
+		});
+	};
+	oldFirebase.editTodo = function (apiKeys, itemId, editedItem) {
+		return new Promise((resolve, reject) =>{
+			$.ajax({
+				method:"PUT",
+				url:`${apiKeys.databaseURL}/items/${itemId}.json`,
+				data: JSON.stringify(editedItem),
+				dataType:"json"
+			}).then((response)=>{
+				console.log("put",response);
+				resolve(response);
+			},(error)=>{
+				reject(error);
+			});
+
+		});
+	};
 	return oldFirebase;
 
 })(FbAPI || {});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,157 @@
 "use strict";	
-let apiKeys ={};
+
+let apiKeys = {};
+let uid ="";
+
+function putTodoInDOM (){
+  FbAPI.getTodos(apiKeys, uid).then(function(items){
+      console.log("items from FB", items);
+      $('#completed-tasks').html("");
+      $('#incomplete-tasks').html("");
+      items.forEach(function(item){
+        if(item.isCompleted === true){
+          let newListItem = `<li data-completed="${item.isCompleted}">`;
+          newListItem+=`<div class="col-xs-8" data-fbid="${item.id}">`;
+          newListItem+='<input class="checkboxStyle" type="checkbox" checked>';
+          newListItem+=`<label class="inputLabel">${item.task}</label>`;
+          newListItem+='<input type="text" class="inputTask">';
+          newListItem+='</div>';
+          newListItem+='</li>';
+          //apend to list
+          $('#completed-tasks').append(newListItem);
+        } else {
+          let newListItem = `<li data-completed="${item.isCompleted}">`;
+          newListItem+=`<div class="col-xs-8" data-fbid="${item.id}">`;
+          newListItem+='<input class="checkboxStyle" type="checkbox">';
+          newListItem+=`<label class="inputLabel">${item.task}</label>`;
+          newListItem+='<input type="text" class="inputTask">';
+          newListItem+='</div>';
+          newListItem+='<div class="col-xs-4">';
+          newListItem+=`<button class="btn btn-default col-xs-6 edit" data-fbid="${item.id}">Edit</button>`;
+          newListItem+=`<button class="btn btn-danger col-xs-6 delete"  data-fbid="${item.id}">Delete</button> `;
+          newListItem+='</div>';
+          newListItem+='</li>';
+          //apend to list
+          $('#incomplete-tasks').append(newListItem);
+        }
+
+      });
+    });
+}
 
 $(document).ready(function(){
-	FbAPI.firebaseCredentials().then(function(keys){
-	console.log("keys",keys);
-	apiKeys = keys;
-	firebase.initializeApp(apiKeys);
-	FbAPI.getTodos(apiKeys).then(function(items) {
-		console.log("items from FB",items);
+  FbAPI.firebaseCredentials().then(function(keys){
+    console.log("keys", keys);
+    apiKeys = keys;
+    firebase.initializeApp(apiKeys);
+  });
+
+  $('#add-todo-button').on('click', function(){
+    console.log("clicked new todo button");
+    let newItem = {
+      "task": $('#add-todo-text').val(),
+      "isCompleted" : false,
+      "uid": uid
+    };
+    FbAPI.addTodo(apiKeys, newItem).then(function(){
+      putTodoInDOM();
+    });
+  });
+
+
+$('ul').on("click", ".delete", function(){
+  let itemId = $(this).data("fbid");
+  FbAPI.deleteTodo(apiKeys, itemId).then(function(){
+    putTodoInDOM();
+  });
+});
+
+ $("ul").on("click", ".edit", function() {
+	let itemId = $(this).data("fbid");
+	console.log("itemId",itemId );	
+	let parent = $(this).closest("li");
+	console.log("parent",parent );
+	if(!parent.hasClass("editMode")){
+		parent.addClass("editMode");
+	}else{
+		let editedItem ={
+			"task": parent.find(".inputTask").val(),
+			"isCompleted": false,
+			"uid": uid
+		};
+	  FbAPI.editTodo(apiKeys, itemId, editedItem).then(function(){
+		parent.removeClass("editMode");
+	    putTodoInDOM();
+	  });
+	}
+});
+
+$('ul').on("change", 'input[type="checkbox"]', function(){
+	let updatedIsCompleted = $(this).closest("li").data("completed");
+	let itemId = $(this).parent().data("fbid");
+	let task = $(this).siblings(".inputLabel").html();
+
+
+	let editedItem = {
+		"task":task,
+		"isCompleted": !updatedIsCompleted,
+		"uid": uid
+	};
+	FbAPI.editTodo(apiKeys, itemId, editedItem).then(function () {
+		putTodoInDOM();
+	});
+
+});
+
+
+
+
+$("#registerButton").on("click", function(){
+	let email = $("#inputEmail").val();
+	let password = $("#inputPassword").val();
+	let username = $("#inputUsername").val();
+	let user = {
+		"email": email,
+		"password": password
+	};
+	FbAPI.registerUser(user).then(function(registerResponse){
+		console.log("register response", registerResponse);
+		let uid = registerResponse;
+		let newUser = {
+			"username": username,
+			"uid": registerResponse.uid
+		};
+		return FbAPI.addUser(apiKeys, newUser);
+	}).then(function(addUserResponse){
+		return FbAPI.loginUser(user);
+	}).then(function(loginResponse){
+		console.log("loginResponse",loginResponse);
+		uid = loginResponse.uid;
+    	putTodoInDOM();
+    	$("login-container").addClass("hide");
+    	$("todo-container").removeClass("hide");
+
 	});
 });
 
+$("#loginButton").on("click", function(){
+	let email = $("#inputEmail").val();
+	let password = $("#inputPassword").val();
 
+	let user = {
+		"email": email,
+		"password": password
+	};
+	FbAPI.loginUser(user).then(function(loginResponse){
+		uid = loginResponse.uid;
+    	putTodoInDOM(uid);
+    	$("#login-container").addClass("hide");
+    	$("#todo-container").removeClass("hide");
 
-
+	});
 });
 
-
-
-
-
-
-
-
+});
 
 
 
